a8bec46ab7642567fe2e077d04a54656e7be1a33
quandx <quandx@kaopiz.com>
2020-08-17 16:34:49 +0700

    add navigation bottom bar

--- App.tsx
+++ App.tsx
@@ -10,12 +10,22 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HoneScreen from './src/HomeScreen';
+import DetailScreen from './src/DetailScreen';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Tab = createBottomTabNavigator();
 
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      <Text style={styles.body}>App</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HoneScreen} />
+        <Tab.Screen name="Settings" component={DetailScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
